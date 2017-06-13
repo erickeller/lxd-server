c450519d7c7ce9543a85c0f7bd7eb131df3daaa7
Eric Keller <keller.eric@gmail.com.com>
2017-06-13 08:50:21 +0200

    use ppa latest greatest version of lxd

--- client_provision.sh
+++ client_provision.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Installing LXD and setting it up..."
+sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable -y
 apt-get update >/dev/null 2>&1
 apt-get dist-upgrade -y >/dev/null 2>&1
 apt-get install -y lxd lxc >/dev/null 2>&1
