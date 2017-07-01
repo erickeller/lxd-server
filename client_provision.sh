edaae34390ba081de22fe8fd27f26b9989b560b9
Eric Keller <keller.eric@gmail.com.com>
2017-07-01 19:38:43 +0200

    use lxd init to create a default zfs storage pool

--- client_provision.sh
+++ client_provision.sh
@@ -3,7 +3,8 @@ echo "Installing LXD and setting it up..."
 sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable -y
 apt-get update >/dev/null 2>&1
 apt-get dist-upgrade -y >/dev/null 2>&1
-apt-get install -y lxd lxc >/dev/null 2>&1
+apt-get install -y lxd lxc zfsutils-linux >/dev/null 2>&1
 adduser ubuntu lxd
 lxc list
+lxd init
 lxc config set core.https_address "[::]:8443"
